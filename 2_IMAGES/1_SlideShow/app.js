62de49a21077c0b879a52cca92ba16441b6feff8
Samruddh pk <107286956+Samruddhpk@users.noreply.github.com>
2024-08-09 16:09:23 +0530

    added auto slide functionality

--- 2_IMAGES/1_SlideShow/app.js
+++ 2_IMAGES/1_SlideShow/app.js
@@ -47,3 +47,19 @@ function showSlides(n) {
 
 
 
+// auto slide
+
+// function showSlidesAuto() {
+//     let i;
+//     let slides = document.getElementsByClassName("slide");
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     slideIndex++;
+//     if (slideIndex > slides.length) { slideIndex = 1; }
+//     slides[slideIndex - 1].style.display = "block";
+//     setTimeout(showSlidesAuto, 3000); // Change image every 2 seconds
+// }
+
+
+// showSlidesAuto();
