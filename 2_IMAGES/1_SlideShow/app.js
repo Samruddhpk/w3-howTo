9ca2a7b9fd91e885f400499a917ccd16a66dcb55
Samruddh pk <107286956+Samruddhpk@users.noreply.github.com>
2024-08-09 14:48:56 +0530

    slideshow

---
+++ 2_IMAGES/1_SlideShow/app.js
@@ -0,0 +1,49 @@
+const slides = document.getElementsByClassName("slide");
+const dots = document.getElementsByClassName("dot");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+
+
+// next / previous controls
+
+
+prevBtn.addEventListener("click", () => {
+    showSlides(slideIndex -= 1);
+});
+
+nextBtn.addEventListener("click", () => {
+    showSlides(slideIndex += 1);
+});
+
+// dots
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+
+
